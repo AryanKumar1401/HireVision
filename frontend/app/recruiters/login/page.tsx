36f53abdf51bfe600e4f646b9526db9b763ec502
23spakkerakari <pradhi.pakkerakari@gmail.com>
2025-04-07 22:55:05 -0400

    latest changes with half-functioning oauth

--- frontend/app/recruiters/login/page.tsx
+++ frontend/app/recruiters/login/page.tsx
@@ -18,7 +18,7 @@ export default function RecruiterSignInPage() {
       const formData = new FormData(e.currentTarget)
       const email = formData.get('email') as string
       const password = formData.get('password') as string
-  
+      
       const { data, error } = await supabase.auth.signInWithPassword({
         email,
         password
